a415641e0a75a791ddbdade4225ce83254d08f6e
Jefferson Wu <jeffersonw888@gmail.com>
2026-04-03 17:19:53 -0500

    created firebase connection

---
+++ src/dataconnect-generated/react/esm/index.esm.js
@@ -0,0 +1,56 @@
+import {
+  listCourseOfferingsRef,
+  getCourseOfferingRef,
+  createCourseOfferingRef,
+  createQuestionRef,
+  connectorConfig,
+} from '../../esm/index.esm.js';
+import { validateArgs, CallerSdkTypeEnum } from 'firebase/data-connect';
+import {
+  useDataConnectQuery,
+  useDataConnectMutation,
+  validateReactArgs,
+} from '@tanstack-query-firebase/react/data-connect';
+
+export function useListCourseOfferings(dcOrOptions, options) {
+  const { dc: dcInstance, options: inputOpts } = validateReactArgs(
+    connectorConfig,
+    dcOrOptions,
+    options,
+  );
+  const ref = listCourseOfferingsRef(dcInstance);
+  return useDataConnectQuery(ref, inputOpts, CallerSdkTypeEnum.GeneratedReact);
+}
+
+export function useGetCourseOffering(dcOrVars, varsOrOptions, options) {
+  const {
+    dc: dcInstance,
+    vars: inputVars,
+    options: inputOpts,
+  } = validateReactArgs(connectorConfig, dcOrVars, varsOrOptions, options, true, true);
+  const ref = getCourseOfferingRef(dcInstance, inputVars);
+  return useDataConnectQuery(ref, inputOpts, CallerSdkTypeEnum.GeneratedReact);
+}
+export function useCreateCourseOffering(dcOrOptions, options) {
+  const { dc: dcInstance, vars: inputOpts } = validateArgs(
+    connectorConfig,
+    dcOrOptions,
+    options,
+  );
+  function refFactory(vars) {
+    return createCourseOfferingRef(dcInstance, vars);
+  }
+  return useDataConnectMutation(refFactory, inputOpts, CallerSdkTypeEnum.GeneratedReact);
+}
+
+export function useCreateQuestion(dcOrOptions, options) {
+  const { dc: dcInstance, vars: inputOpts } = validateArgs(
+    connectorConfig,
+    dcOrOptions,
+    options,
+  );
+  function refFactory(vars) {
+    return createQuestionRef(dcInstance, vars);
+  }
+  return useDataConnectMutation(refFactory, inputOpts, CallerSdkTypeEnum.GeneratedReact);
+}
