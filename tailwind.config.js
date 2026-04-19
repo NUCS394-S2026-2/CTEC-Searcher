850ee1c3cbb3f5bb93a65041373db2de0179f199
yiminhuang <yminh@dhcp-10-105-248-206.wireless.northwestern.private>
2026-04-19 16:47:36 -0500

    Update colors to Northwestern Purple (#4E2A84)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          50: '#f9f5fc',
+          100: '#f1e8f7',
+          200: '#dfcded',
+          300: '#c6aaea',
+          400: '#a87fc1',
+          500: '#8e57b3',
+          600: '#743ea4',
+          700: '#4E2A84', // Northwestern Purple
+          800: '#43266f',
+          900: '#38225a',
+          950: '#251241',
+        },
+      },
+    },
   },
   plugins: [],
 };
